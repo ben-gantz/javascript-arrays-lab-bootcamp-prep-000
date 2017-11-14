ef206c51e88bdefd5af9cb5602ca33d6b3053e90
Ben Gantz <bengantz@gmail.com>
2017-11-14 07:58:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,19 @@ function destructivelyRemoveLastKitten() {
 function destructivelyRemoveFirstKitten() {
   kittens.shift()
 }
+
+function appendKitten(newKitten) {
+  return [...kittens, `${newKitten}`]
+}
+
+function prependKitten(newKitten) {
+  return [`${newKitten}`, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
